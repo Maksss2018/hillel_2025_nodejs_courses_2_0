15998ebf92b732299d3a9f7c9724800be357b4f5
Maksym Malyi <malyimaksym37@gmail.com>
2026-03-30 11:51:48 +0300

    updates for schema

--- models/schemaGoods.js
+++ models/schemaGoods.js
@@ -1,31 +1,54 @@
+const noEmptyString = "^(?!\\s*$).+";
 const schema = {
   type: "object",
   properties: {
-    id: { type: "string", minLength: 6, maxLength: 32 },
-    name: { type: "string", minLength: 3, maxLength: 255 },
-    brand: { type: "string" },
-    category: { type: "string", minLength: 3 },
-    gender: { type: "string", pattern: "Men|Women|Unisex" },
-    price: { type: "number" },
-    currency: { type: "string", pattern: "UAH|USD" },
-    colors: { type: "array", items: { type: "string" } },
-    sizes: { type: "array", items: { type: "string" } },
+    id: {
+      type: "string",
+      minLength: 6,
+      maxLength: 32,
+      pattern: noEmptyString,
+    },
+    name: {
+      type: "string",
+      minLength: 3,
+      maxLength: 255,
+      pattern: noEmptyString,
+    },
+    brand: { type: "string", pattern: noEmptyString },
+    category: { type: "string", minLength: 3, pattern: noEmptyString },
+    gender: { type: "string", pattern: "^(Men|Women|Unisex)$" },
+    price: { type: "number", minimum: 1 },
+    currency: { type: "string", pattern: "^(UAH|USD)$" },
+    colors: {
+      type: "array",
+      items: { type: "string", pattern: "^(Black|Gray|Beige|White|Yellow)$" },
+    },
+    sizes: {
+      type: "array",
+      items: { type: "string", pattern: "^(XS|S|M|L|XL)$" },
+    },
     material: {
       type: "object",
       properties: {
-        main: { type: "string" },
-        composition: { type: "string" },
+        main: { type: "string", pattern: noEmptyString },
+        composition: { type: "string", pattern: noEmptyString },
       },
     },
-    description: { type: "string" },
-    care_instruction: { type: "array", items: { type: "string" } },
-    stock: { type: "number" },
-    rating: { type: "number" },
-    reviews_count: { type: "number" },
-    sku: { type: "string" },
+    description: { type: "string", pattern: noEmptyString },
+    care_instruction: {
+      type: "array",
+      items: { type: "string", pattern: noEmptyString },
+    },
+    stock: { type: "number", minimum: 0 },
+    rating: { type: "number", minimum: 0 },
+    reviews_count: { type: "number", minimum: 0 },
+    sku: { type: "string", pattern: noEmptyString },
     images: {
       type: "array",
-      items: { type: "string" },
+      items: {
+        type: "string",
+        pattern: "^https?:\\/\\/[a-z0-9.-]+\\/.+\\.(jpg|jpeg|png|gif|webp)$",
+      },
     },
   },
   required: [
